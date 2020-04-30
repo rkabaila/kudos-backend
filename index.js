54f38262ca37266d9bfde0cd0732fc9427a62994
Rimas Kabaila <rimas.kabaila@gmail.com>
2020-04-30 16:52:36 +0300

    added role to user model, updated PRISMA_MANAGEMENT_API_SECRET naming

--- index.js
+++ index.js
@@ -27,14 +27,14 @@ const resolvers = {
     userOwnKudoses(root, args, context) {
       return context.prisma
         .user({
-          id: args.userId
+          id: args.userId,
         })
         .ownKudoses();
     },
     userWrittenKudoses(root, args, context) {
       return context.prisma
         .user({
-          id: args.userId
+          id: args.userId,
         })
         .writtenKudoses();
     },
@@ -46,18 +46,18 @@ const resolvers = {
         throw new Error("Not Authenticated");
       }
       return context.prisma.users();
-    }
+    },
   },
   Mutation: {
     addKudos(root, args, context) {
       return context.prisma.createKudos({
         text: args.text,
         author: {
-          connect: { id: args.authorId }
+          connect: { id: args.authorId },
         },
         recipient: {
-          connect: { id: args.recipientId }
-        }
+          connect: { id: args.recipientId },
+        },
       });
     },
     deleteKudos(root, args, context) {
@@ -68,7 +68,8 @@ const resolvers = {
       return await context.prisma.createUser({
         slackId: args.slackId,
         name: args.name,
-        password: hashedPassword
+        role: args.role,
+        password: hashedPassword,
       });
     },
     async login(root, args, context) {
@@ -83,57 +84,58 @@ const resolvers = {
       const token = jwt.sign(
         {
           id: user.id,
-          name: user.name
+          name: user.name,
+          role: user.role,
         },
         jwtSecret,
         {
-          expiresIn: "30d"
+          expiresIn: "30d",
         }
       );
       return {
         token,
-        user
+        user,
       };
     },
     deleteUser(root, args, context) {
       return context.prisma.deleteUser({ id: args.id });
-    }
+    },
   },
   User: {
     ownKudoses(root, args, context) {
       return context.prisma
         .user({
-          id: root.id
+          id: root.id,
         })
         .ownKudoses();
     },
     writtenKudoses(root, args, context) {
       return context.prisma
         .user({
-          id: root.id
+          id: root.id,
         })
         .writtenKudoses();
-    }
+    },
   },
   Kudos: {
     author(root, args, context) {
       return context.prisma
         .kudos({
-          id: root.id
+          id: root.id,
         })
         .author();
     },
     recipient(root, args, context) {
       return context.prisma
         .kudos({
-          id: root.id
+          id: root.id,
         })
         .recipient();
-    }
-  }
+    },
+  },
 };
 
-const authenticateUser = token => {
+const authenticateUser = (token) => {
   try {
     if (token) {
       return jwt.verify(token, jwtSecret);
@@ -147,16 +149,16 @@ const authenticateUser = token => {
 const server = new GraphQLServer({
   typeDefs: "./schema.graphql",
   resolvers,
-  context: req => {
+  context: (req) => {
     const tokenWithBearer = req.request.headers.authorization || "";
     const token = tokenWithBearer.split(" ")[1];
     const authenticatedUser = authenticateUser(token);
 
     return {
       authenticatedUser,
-      prisma
+      prisma,
     };
-  }
+  },
 });
 const expressServer = server.express.use(
   bodyParser.urlencoded({ extended: true })
@@ -177,12 +179,12 @@ expressServer.post("/command", async (req, res) => {
       type: "modal",
       title: {
         type: "plain_text",
-        text: "Kudos info"
+        text: "Kudos info",
       },
       callback_id: "submit-kudos",
       submit: {
         type: "plain_text",
-        text: "Send kudos"
+        text: "Send kudos",
       },
       blocks: [
         {
@@ -190,42 +192,42 @@ expressServer.post("/command", async (req, res) => {
           type: "input",
           label: {
             type: "plain_text",
-            text: "Kudos text:"
+            text: "Kudos text:",
           },
           element: {
             action_id: "kudos_text",
             type: "plain_text_input",
             initial_value: slackRequest.text,
-            multiline: true
-          }
+            multiline: true,
+          },
         },
         {
           block_id: "recipient_block",
           type: "input",
           label: {
             type: "plain_text",
-            text: "Send to user:"
+            text: "Send to user:",
           },
           element: {
             action_id: "recipient",
             type: "users_select",
             placeholder: {
               type: "plain_text",
-              text: "Select user"
-            }
-          }
-        }
-      ]
-    })
+              text: "Select user",
+            },
+          },
+        },
+      ],
+    }),
   };
 
   axios
     .post(`${apiUrl}/views.open`, qs.stringify(view))
-    .then(result => {
+    .then((result) => {
       // console.log("views.open:", result.data);
       res.send("");
     })
-    .catch(err => {
+    .catch((err) => {
       // console.log("views.open call failed:", err);
       res.sendStatus(500);
     });
@@ -249,22 +251,22 @@ expressServer.post("/interaction", async (req, res) => {
   );
 
   const users = await prisma.users();
-  const author = users.find(user => user.slackId === authorSlackId);
-  const recipient = users.find(user => user.slackId === recipientSlackId);
+  const author = users.find((user) => user.slackId === authorSlackId);
+  const recipient = users.find((user) => user.slackId === recipientSlackId);
 
   if (author && recipient && kudosText) {
     const addedKudos = await prisma.createKudos({
       text: kudosText,
       author: {
-        connect: { id: author.id }
+        connect: { id: author.id },
       },
       recipient: {
-        connect: { id: recipient.id }
-      }
+        connect: { id: recipient.id },
+      },
     });
     res.send("");
     axios.post(webhookUrl, {
-      text: `${addedKudos.text} is sent to ${recipient.name}`
+      text: `${addedKudos.text} is sent to ${recipient.name}`,
     });
   } else {
     res.sendStatus(500);
